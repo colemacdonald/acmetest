b70a79bb7b5da1d6294fed69d28f1972b937144a
Edgardo Cuello <ecuellog@gmail.com>
2017-06-13 23:11:06 -0700

    Create RevealPasswordTest.java
    Changed to correct naming of class according to Java convention.

--- RevealPasswordTest.java
+++ RevealPasswordTest.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import seng426.Helper;
 
-public class reveal_password {
+public class RevealPasswordTest {
 
 	private WebDriver driver;
 	private String baseUrl = "http://localhost:8080";
